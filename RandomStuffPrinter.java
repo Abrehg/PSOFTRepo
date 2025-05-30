d602e2831e93e7e751cd36335818d928ac68f198
Abrehg <adityaasuratkal@gmail.com>
2025-05-30 19:41:10 -0400

    Create RandomStuffPrinter.java

---
+++ RandomStuffPrinter.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import java.util.UUID;
+
+public class RandomStuffPrinter {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        System.out.println("Hello! Here's some random stuff for you:");
+        System.out.println("---------------------------------------");
+
+        // 1. A random integer
+        int randomNumber = random.nextInt(1000); // Random int between 0 (inclusive) and 1000 (exclusive)
+        System.out.println("A random integer: " + randomNumber);
+
+        // 2. A random double
+        double randomDouble = random.nextDouble() * 100; // Random double between 0.0 (inclusive) and 100.0 (exclusive)
+        System.out.println("A random double: " + String.format("%.2f", randomDouble));
+
+        // 3. A random boolean
+        boolean randomBoolean = random.nextBoolean();
+        System.out.println("A random boolean: " + randomBoolean);
+
+        // 4. A random character (from A-Z)
+        char randomChar = (char) (random.nextInt(26) + 'A');
+        System.out.println("A random uppercase letter: " + randomChar);
+
+        // 5. A random string (UUID)
+        String randomUUID = UUID.randomUUID().toString();
+        System.out.println("A random UUID: " + randomUUID);
+
+        // 6. A random selection from a predefined list
+        String[] randomWords = {"Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape"};
+        String randomWord = randomWords[random.nextInt(randomWords.length)];
+        System.out.println("A random word from a list: " + randomWord);
+
+        // 7. A longer random string
+        int length = 10 + random.nextInt(11); // Random length between 10 and 20
+        StringBuilder randomString = new StringBuilder();
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (int i = 0; i < length; i++) {
+            randomString.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        System.out.println("A random alphanumeric string: " + randomString.toString());
+
+        System.out.println("---------------------------------------");
+        System.out.println("That's all the random stuff for now. Bye!");
+    }
+}
