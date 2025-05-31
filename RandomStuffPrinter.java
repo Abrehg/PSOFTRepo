88e159a64236a4efb997848a904eaac9e8e08f5a
Lukas-Petervary <lukas.at.petervary@gmail.com>
2025-05-30 21:04:14 -0400

    hoare triple or wtv

--- RandomStuffPrinter.java
+++ RandomStuffPrinter.java
@@ -4,6 +4,7 @@ public class RandomStuffPrinter {
 
     public static void main(String[] args) {
 	System.out.println("i <3 kuzmin");
+	System.out.println("stronger precondition, weaker postcondition !!");
         System.out.println("Initial change");
     }
 }
